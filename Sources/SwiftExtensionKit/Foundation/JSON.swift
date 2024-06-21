2bd106d661fd7dd91c477e5c3a6af80fc05e8178
Louis Shin <louis@spoonradio.co>
2024-06-21 14:53:37 +0900

    JSON 파일 추가

---
+++ Sources/SwiftExtensionKit/Foundation/JSON.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public enum JSONError: Error {
+    case invalidJSONObject
+    case emptyJSONString
+}
+
+public protocol JSONStringable {
+    func JSONString(options: JSONSerialization.WritingOptions) throws -> String
+}
+
+extension JSONStringable {
+    
+    public func JSONString(options: JSONSerialization.WritingOptions = []) throws -> String  {
+        guard JSONSerialization.isValidJSONObject(self) else {
+            throw JSONError.invalidJSONObject
+        }
+        
+        let data = try JSONSerialization.data(withJSONObject: self, options: options)
+        guard let result = String(data: data, encoding: .utf8) else {
+            throw JSONError.emptyJSONString
+        }
+        
+        return result
+    }
+}
+
+extension Array: JSONStringable {}
+
+extension Dictionary: JSONStringable {}
