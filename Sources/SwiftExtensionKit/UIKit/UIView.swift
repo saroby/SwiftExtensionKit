4eb52552ccdba59390c5dcfce5b22ccc033f2c98
Louis Shin <louis@spoonradio.co>
2024-06-21 11:39:42 +0900

    UIView 클래스 추가

---
+++ Sources/SwiftExtensionKit/UIKit/UIView.swift
@@ -0,0 +1,14 @@
+#if canImport(UIKit)
+import UIKit
+
+extension UIView {
+    
+    public convenience init(size: CGSize) {
+        self.init(frame: .init(origin: .zero, size: size))
+    }
+    
+    public func addSubviews(_ views: UIView ...) {
+        views.forEach { self.addSubview($0) }
+    }
+}
+#endif
