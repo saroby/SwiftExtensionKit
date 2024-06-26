8bb0893fad28d48ae8fd4ca1ed81b88c7fc526e5
Louis Shin <louis@spoonradio.co>
2024-06-26 14:53:13 +0900

    asyncNext의 delay 파라미터에 default값 추가

--- Sources/SwiftExtensionKit/Foundation/DispatchQueue.swift
+++ Sources/SwiftExtensionKit/Foundation/DispatchQueue.swift
@@ -3,7 +3,7 @@ import Foundation
 
 extension DispatchQueue {
     
-    public func asyncNext(_ delay: TimeInterval, execute work: @escaping () -> Void) {
+    public func asyncNext(_ delay: TimeInterval = 0.0, execute work: @escaping () -> Void) {
         DispatchQueue.global().asyncAfter(deadline: .now() + delay) { self.async { work() } }
     }
 }
