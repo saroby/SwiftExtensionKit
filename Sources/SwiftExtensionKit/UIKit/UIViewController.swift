ac676a6a1d0792c154e9161bfdc66a03e4c22ed8
Louis Shin <louis@spoonradio.co>
2024-06-26 11:27:06 +0900

    presented 파라미터 수정

--- Sources/SwiftExtensionKit/UIKit/UIViewController.swift
+++ Sources/SwiftExtensionKit/UIKit/UIViewController.swift
@@ -4,8 +4,8 @@ import UIKit
 
 extension UIViewController {
     
-    public func presented(viewControllerPresent parent: UIViewController? = UIApplication.getTopVC(), animated: Bool, completion: (()-> Void)? = nil) {
-        parent?.present(self, animated: animated, completion: completion)
+    public func presented(viewControllerPresent parent: UIViewController, animated: Bool, completion: (()-> Void)? = nil) {
+        parent.present(self, animated: animated, completion: completion)
     }
 }
 #endif
