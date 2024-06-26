ae211593c3fd002acc34ca68659280d4437f5c43
Louis Shin <louis@spoonradio.co>
2024-06-26 11:47:14 +0900

    presetned 함수에 MainActor 적용

--- Sources/SwiftExtensionKit/UIKit/UIViewController.swift
+++ Sources/SwiftExtensionKit/UIKit/UIViewController.swift
@@ -4,6 +4,7 @@ import UIKit
 
 extension UIViewController {
     
+    @MainActor
     public func presented(viewControllerPresent parent: UIViewController, animated: Bool, completion: (()-> Void)? = nil) {
         parent.present(self, animated: animated, completion: completion)
     }
