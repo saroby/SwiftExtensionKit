841f1817481fdf6b65ee95d23359e39b4c3bb62c
Louis Shin <louis@spoonradio.co>
2024-06-26 14:36:01 +0900

    Task.delayed 함수 추가

---
+++ Sources/SwiftExtensionKit/Foundation/Task.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+
+extension Task where Failure == Error {
+    
+    // reference : https://www.swiftbysundell.com/articles/delaying-an-async-swift-task/
+    @discardableResult 
+    public static func delayed(
+        byTimeInterval delayInterval: TimeInterval,
+        priority: TaskPriority? = nil,
+        operation: @escaping @Sendable () async throws -> Success
+    ) -> Task {
+        Task(priority: priority) {
+            let delay = UInt64(delayInterval * 1_000_000_000)
+            try await Task<Never, Never>.sleep(nanoseconds: delay)
+            return try await operation()
+        }
+    }
+}
