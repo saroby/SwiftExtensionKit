0b5573d5540823ec390c12949ea911d5359f4c96
Louis Shin <saroby@naver.com>
2026-04-22 16:07:37 +0900

    Package Dependencies updated

--- Package.swift
+++ Package.swift
@@ -11,12 +11,15 @@ let package = Package(
     products: [
         .library(
             name: "SwiftExtensionKit",
-            targets: ["SwiftExtensionKit"]),
+            targets: ["SwiftExtensionKit"]
+        ),
     ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift", .upToNextMajor(from: "6.10.2")),
         .package(url: "https://github.com/layoutBox/FlexLayout", branch: "master"),
         .package(url: "https://github.com/siteline/swiftui-introspect", branch: "main"),
+        .package(url: "https://github.com/airbnb/lottie-spm", .upToNextMajor(from: "4.6.0")),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.7.1")),
     ],
     targets: [
         .target(
@@ -26,6 +29,8 @@ let package = Package(
                 .product(name: "RxCocoa", package: "RxSwift"),
                 "FlexLayout",
                 .product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
+                .product(name: "Lottie", package: "lottie-spm"),
+                "SnapKit",
             ]
         ),
         .testTarget(
@@ -36,6 +41,8 @@ let package = Package(
                 .product(name: "RxCocoa", package: "RxSwift"),
                 "FlexLayout",
                 .product(name: "SwiftUIIntrospect", package: "swiftui-introspect"),
+                .product(name: "Lottie", package: "lottie-spm"),
+                "SnapKit",
             ]),
     ]
 )
